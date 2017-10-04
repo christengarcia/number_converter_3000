b888bc63d12aa4af8709452082d358e1bf6ef95b
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-10-04 16:48:59 +1100

    Added number_to_base

--- app/helpers/number_converter_helper.rb
+++ app/helpers/number_converter_helper.rb
@@ -8,4 +8,21 @@ module NumberConverterHelper
         end
         output
     end
+
+    DIGITS = ('0'..'9').to_a + ('a'..'z').to_a
+    def number_to_base(number, base)
+        is_negative = number < 0
+        number = -number if is_negative
+        return '0' if number == 0
+        return 'indeterminate' if base < 2
+        output = ''
+        while number > 0
+            bit = number % base
+            letter = DIGITS[bit]
+            output = letter + output
+            number = number / base
+        end
+        output = '-' + output if is_negative
+        output
+    end
 end
